6a7bdffa7d1bdf6a7ee3300c1e106b1ce591c5ab
Dina Alexandru <alexdina692@gmail.com>
2024-03-18 17:08:38 +0200

    authentication - backend
    Added User model, Roles enum, Authentication Controller, Authentication Routes. After authentication, user will get a JWT Token.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const authRoutes = require("./routes/AuthRoutes");
 
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
